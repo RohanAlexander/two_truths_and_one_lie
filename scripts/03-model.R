b72d7723c0cd70df8b6930c465d674bf0e61a76d
Rohan Alexander <rohan.alexander@utoronto.ca>
2024-03-28 10:05:11 -0400

    Add first model

--- scripts/03-model.R
+++ scripts/03-model.R
@@ -1,26 +1,27 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models the relationship between certainty of a guess and whether it was right
+# Author: Rohan Alexander
+# Date: 28 March 2024
+# Contact: rohan.alexander@utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Need cleaned analysis dataset from 01-data_cleaning.R
+# Any other information needed? -
 
 
 #### Workspace setup ####
+library(arrow)
 library(tidyverse)
 library(rstanarm)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+analysis_data <- read_parquet("data/analysis_data/analysis_data.parquet")
 
 ### Model data ####
 first_model <-
   stan_glm(
-    formula = flying_time ~ length + width,
+    formula = guessed_correct ~ certainty,
     data = analysis_data,
-    family = gaussian(),
+    family = binomial(link = "logit"),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_aux = exponential(rate = 1, autoscale = TRUE),
