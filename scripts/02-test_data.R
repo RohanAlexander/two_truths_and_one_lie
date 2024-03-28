3ac4e4d44c88c4fa5a5596a2f02c226e771fe2f2
Rohan Alexander <rohan.alexander@utoronto.ca>
2024-03-28 10:10:06 -0400

    Add data tests

--- scripts/02-test_data.R
+++ scripts/02-test_data.R
@@ -1,15 +1,29 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests the real analysis data based on our expectations from the simulation.
+# Author: Rohan Alexander
+# Date: 28 March 2024
+# Contact: rohan.alexander@utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Need analysis data from 01-data_cleaning.R
+# Any other information needed? -
 
 
 #### Workspace setup ####
+library(arrow)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Test data ####
+analysis_data <- read_parquet("data/analysis_data/analysis_data.parquet")
+
+# Expect that guessed_correct is only ever 0 or 1.
+max(analysis_data$guessed_correct) == 1
+min(analysis_data$guessed_correct) == 0
+### This test passed, but only after changing order and should be improved.
+any(unique(analysis_data$guessed_correct) == c(1, 0))
+
+# Expect that certainty is only ever an integer 0 to 10, inclusive.
+max(analysis_data$certainty) == 10
+min(analysis_data$certainty) == 0
+any(unique(analysis_data$certainty) == c(0:10))
+
